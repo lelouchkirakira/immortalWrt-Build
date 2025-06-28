0d2690b1fadada52ce5dae7f7b7c4be69b6c339a
小橙子 <615760954@qq.com>
2025-06-28 09:38:36 +0800

    Update custom.sh

--- build/Lean_Cudy_18.04/custom.sh
+++ build/Lean_Cudy_18.04/custom.sh
@@ -63,22 +63,13 @@ else
 fi
 
 #
-sed -i 's#192.168.1.1#192.168.11.41#g' $NET                                                    # 定制默认IP
-# sed -i 's#LEDE#OpenWrt-X86#g' $NET                                                     # 修改默认名称为OpenWrt-X86
-# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                             # 取消系统默认密码
+sed -i 's#192.168.1.1#192.168.3.1#g' $NET                                                    # 定制默认IP
 sed -i "s/LEDE /Built on $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" $ZZZ              # 增加自己个性名称
 echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ                      # 设置默认主题(如果编译可会自动修改默认主题的，有可能会失效)
 
 # ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
 
 sed -i 's#localtime  = os.date()#localtime  = os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")#g' package/lean/autocore/files/*/index.htm               # 修改默认时间格式
-sed -i 's#%D %V, %C#%D %V, %C Lean_x86_64#g' package/base-files/files/etc/banner               # 自定义banner显示
-# sed -i 's@list listen_https@# list listen_https@g' package/network/services/uhttpd/files/uhttpd.config               # 停止监听443端口
-# sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config               # 修改流量统计写入为10分钟
-# sed -i 's#option database_generations 10#option database_generations 3#g' feeds/packages/net/nlbwmon/files/nlbwmon.config               # 修改流量统计数据周期
-# sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config               # 修改流量统计数据存放默认位置
-# sed -i 's#interval: 5#interval: 1#g' feeds/luci/applications/luci-app-wrtbwmon/htdocs/luci-static/wrtbwmon/wrtbwmon.js               # wrtbwmon默认刷新时间更改为1秒
-sed -i '/exit 0/i\ethtool -s eth0 speed 10000 duplex full' package/base-files/files//etc/rc.local               # 强制显示2500M和全双工（默认PVE下VirtIO不识别）
 
 # ●●●●●●●●●●●●●●●●●●●●●●●●定制部分●●●●●●●●●●●●●●●●●●●●●●●● #
 
@@ -94,26 +85,26 @@ EOF
 
 cat >> $ZZZ <<-EOF
 # 设置网络-旁路由模式
-uci set network.lan.gateway='192.168.11.1'                     # 旁路由设置 IPv4 网关
-uci set network.lan.dns='114.114.114.114'            # 旁路由设置 DNS(多个DNS要用空格分开)
-uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
-uci delete network.lan.type                                  # 旁路由桥接模式-禁用
-uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
-uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+# uci set network.lan.gateway='192.168.11.1'                     # 旁路由设置 IPv4 网关
+# uci set network.lan.dns='114.114.114.114'            # 旁路由设置 DNS(多个DNS要用空格分开)
+# # uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
+# uci delete network.lan.type                                  # 旁路由桥接模式-禁用
+# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
 
 # 设置防火墙-旁路由模式
-uci set firewall.@defaults[0].syn_flood='0'                  # 禁用 SYN-flood 防御
-uci set firewall.@defaults[0].flow_offloading='0'           # 禁用基于软件的NAT分载
-uci set firewall.@defaults[0].flow_offloading_hw='0'       # 禁用基于硬件的NAT分载
-uci set firewall.@defaults[0].fullcone='0'                   # 禁用 FullCone NAT
-uci set firewall.@defaults[0].fullcone6='0'                  # 禁用 FullCone NAT6
-uci set firewall.@zone[0].masq='1'                             # 启用LAN口 IP 动态伪装
+# uci set firewall.@defaults[0].syn_flood='0'                  # 禁用 SYN-flood 防御
+# uci set firewall.@defaults[0].flow_offloading='0'           # 禁用基于软件的NAT分载
+# uci set firewall.@defaults[0].flow_offloading_hw='0'       # 禁用基于硬件的NAT分载
+# uci set firewall.@defaults[0].fullcone='0'                   # 禁用 FullCone NAT
+# uci set firewall.@defaults[0].fullcone6='0'                  # 禁用 FullCone NAT6
+# uci set firewall.@zone[0].masq='1'                             # 启用LAN口 IP 动态伪装
 
 # 旁路IPV6需要全部禁用
-uci del network.lan.ip6assign                                 # IPV6分配长度-禁用
-uci del dhcp.lan.ra                                             # 路由通告服务-禁用
-uci del dhcp.lan.dhcpv6                                        # DHCPv6 服务-禁用
-uci del dhcp.lan.ra_management                               # DHCPv6 模式-禁用
+# uci del network.lan.ip6assign                                 # IPV6分配长度-禁用
+# uci del dhcp.lan.ra                                             # 路由通告服务-禁用
+# uci del dhcp.lan.dhcpv6                                        # DHCPv6 服务-禁用
+# uci del dhcp.lan.ra_management                               # DHCPv6 模式-禁用
 
 # 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
 uci set network.ipv6=interface
@@ -1035,6 +1026,11 @@ CONFIG_PACKAGE_luci-app-firewall=y
 CONFIG_PACKAGE_luci-app-mosdns=y
 CONFIG_PACKAGE_luci-app-nlbwmon=n
 CONFIG_PACKAGE_luci-app-openclash=y
+CONFIG_PACKAGE_luci-app-accesscontrol=n
+CONFIG_PACKAGE_luci-app-arpbind=n
+CONFIG_PACKAGE_luci-app-ddns=n
+CONFIG_PACKAGE_luci-app-vlmcsd=n
+CONFIG_PACKAGE_luci-app-vsftpd=n
 CONFIG_PACKAGE_iptables-mod-tproxy=y
 CONFIG_PACKAGE_iptables-mod-extra=y
 CONFIG_PACKAGE_ipset=y
