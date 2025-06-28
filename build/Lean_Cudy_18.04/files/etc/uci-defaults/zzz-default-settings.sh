73c0fcc668efa05b3d3bd8b59a90cc716897ccdc
小橙子 <615760954@qq.com>
2025-06-28 12:09:25 +0800

    Update zzz-default-settings.sh

--- build/Lean_Cudy_18.04/files/etc/uci-defaults/zzz-default-settings.sh
+++ build/Lean_Cudy_18.04/files/etc/uci-defaults/zzz-default-settings.sh
@@ -4,19 +4,10 @@ uci set system.@system[0].hostname='QWRT'
 # 设置默认主题
 uci set luci.main.mediaurlbase='/luci-static/argon' && uci commit luci
 
-# 设置登录地址192.168.11.41
-uci set network.lan.ipaddr='192.168.11.41'
+# 设置登录地址192.168.3.1
+uci set network.lan.ipaddr='192.168.3.1'
 uci commit network
 /etc/init.d/network restart
-
-# 添加密钥登录
-mkdir -p /etc/dropbear
-cat << 'EOF' > /etc/dropbear/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5SRidt7m4PX/4KmA7lBx2DLhSss+fKvK+UurPZFszo root@Z4Pro
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDt8P9E0E99vPtY2n8SfOEV469/Yo2QTSYP3qEKY93e7 Public
-EOF
-chmod 600 /etc/dropbear/authorized_keys
-/etc/init.d/dropbear restart
 
 # 删除README
 rm -rf /README
